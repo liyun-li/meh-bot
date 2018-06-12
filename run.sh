60da11031ef2f410d88506ea3f92e376866a686d
Liyun Li <liyun-li@users.noreply.github.com>
2018-06-11 18:07:46 -0700

    better run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build . -t meh-bot
-docker kill meh-bot && docker rm meh-bot 2>/dev/null
+docker rm -f meh-bot 2>/dev/null
 docker run --name meh-bot -d meh-bot
 echo 'Meh bot is running. Go do whatever'
