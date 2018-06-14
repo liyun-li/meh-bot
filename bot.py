df9f11a2e590ce78c25b05fa2d3d1b0029e42d91
Liyun Li <liyun-li@users.noreply.github.com>
2018-06-14 09:41:02 -0700

    Better(?) resource management

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from pathlib import Path  # python3 only
 import sys
 import logging
 
-logging.basicConfig(filename='meh.log',level=logging.DEBUG)
+logging.basicConfig(filename='meh.log',level=logging.INFO)
 
 env_path = Path('.') / '.env'
 
@@ -39,35 +39,15 @@ SIGNIN_USER_ID = 'user'
 SIGNIN_PASS_ID = 'password'
 SIGNIN_BUTTON_SELECTOR = 'button[class="primary"]'
 
-# set up cursor
-options = Options()
-options.set_headless(True)
-# options.set_headless(False) # for debugging
 
-choices = ['chromium-browser', 'google-chrome', 'firefox']
-browser_choice = getenv('BROWSER_CHOICE')
-if browser_choice not in choices:
-    browser = www.Firefox(firefox_options=options, executable_path='./geckodriver')
-elif browser_choice.lower() == choices[2]:
-    browser = www.Firefox(firefox_options=options, executable_path='./geckodriver')
-elif browser_choice.lower() in choices[:2]:
-    options.binary_location = '/usr/bin/' + browser_choice
-    browser = www.Chrome(chrome_options=options, executable_path='./chromedriver')
-
-def send_product_info():
+def send_product_info(sms_body, img):
     '''Send pics'''
-    global browser
-
     # twilio credentials
     ACCOUNT_SID = getenv('TWILIO_ACCOUNT_SID')
     AUTH_TOKEN = getenv('TWILIO_AUTH_TOKEN')
     TO_NUMBER = getenv('TWILIO_TO_NUMBER')
     FROM_NUMBER = getenv('TWILIO_FROM_NUMBER')
 
-    # construct message body
-    sms_body = 'Meh:\n'
-    sms_body += browser.find_element_by_css_selector(PRODUCT_TITLE_SELECTOR).text
-    img = browser.find_element_by_id(PRODUCT_PHOTO_ID).get_attribute('src')
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
     message = client.messages.create(
         to=TO_NUMBER,
@@ -78,46 +58,74 @@ def send_product_info():
 
 def meh_function():
     '''Does what project is supposed to achieve'''
-    global browser
-
-    # driver wait
-    wait = WebDriverWait(browser, 10)  # maximum wait time
-
-    browser.get(SIGNIN)
-
-    signin_button = wait.until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, SIGNIN_BUTTON_SELECTOR)
-    ))
-    signin_input_user = browser.find_element_by_id(SIGNIN_USER_ID)
-    signin_input_pass = browser.find_element_by_id(SIGNIN_PASS_ID)
-
-    print('Signing in...')
-    logging.info('Signing in...')
-    signin_input_user.clear()
-    signin_input_pass.clear()
-    signin_input_user.send_keys(getenv('MEH_USER'))
-    signin_input_pass.send_keys(getenv('MEH_PASS'))
-    signin_button.click()
-
-    flipper = wait.until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, FLIPPER_SELECTOR)
-    ))
-
     try:
+        # set up cursor
+        options = Options()
+        options.set_headless(True)
+        # options.set_headless(False) # for debugging
+
+        choices = ['chromium-browser', 'google-chrome', 'firefox']
+        browser_choice = getenv('BROWSER_CHOICE')
+        if browser_choice not in choices:
+            browser = www.Firefox(firefox_options=options, executable_path='./geckodriver')
+        elif browser_choice.lower() == choices[2]:
+            browser = www.Firefox(firefox_options=options, executable_path='./geckodriver')
+        elif browser_choice.lower() in choices[:2]:
+            options.binary_location = '/usr/bin/' + browser_choice
+            browser = www.Chrome(chrome_options=options, executable_path='./chromedriver')
+
+        # driver wait
+        wait = WebDriverWait(browser, 10)  # maximum wait time
+
+        # First sign in
+        browser.get(SIGNIN)
+
+        signin_button = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, SIGNIN_BUTTON_SELECTOR)
+        ))
+        signin_input_user = browser.find_element_by_id(SIGNIN_USER_ID)
+        signin_input_pass = browser.find_element_by_id(SIGNIN_PASS_ID)
+
+        print('Signing in...')
+        logging.info('Signing in...')
+        signin_input_user.clear()
+        signin_input_pass.clear()
+        signin_input_user.send_keys(getenv('MEH_USER'))
+        signin_input_pass.send_keys(getenv('MEH_PASS'))
+        signin_button.click()
+
+        # the meh icon, they call it a flipper
+        flipper = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, FLIPPER_SELECTOR)
+        ))
+
         browser.find_element_by_css_selector(FLIPPED_SELECTOR)
         print('Icon already flipped')
         logging.info('Icon already flipped')
-    except:
+    except NSE as nse:
         print('Flipping the flipper')
         logging.info('Flipping the flipper')
         flipper.click()
+    except KeyboardInterrupt:
+        print('\n')
+        logging.info('KeyboardInterrupt')
+        browser.quit()
+        exit(0)
+    except Exception as e:
+        logging.error(e)
+        browser.quit()
+        exit(1337)
     finally: # optional text alert
         SEND_SMS = getenv('SEND_SMS')
         if SEND_SMS:
             print('Texting product...')
             logging.info('Texting product...')
-            send_product_info()
-    sleep(3)
+            # construct message body
+            sms_body = 'Meh:\n'
+            sms_body += browser.find_element_by_css_selector(PRODUCT_TITLE_SELECTOR).text
+            img = browser.find_element_by_id(PRODUCT_PHOTO_ID).get_attribute('src')
+            send_product_info(sms_body, img)
+        browser.quit()
 
 def seconds_till_tomorrow():
     n = datetime.utcnow() - timedelta(hours=4)
@@ -145,20 +153,13 @@ if __name__ == '__main__':
         print('Run it only once')
         logging.info('Run it only once')
         meh_function()
-        browser.quit()
         exit(0)
 
-    try:
-        while True:
+    while True:
+        try:
             countdown()
-            sleep(10)
-            meh_function()
-    except KeyboardInterrupt:
-        print('\n')
-        logging.info('KeyboardInterrupt')
-        browser.quit()
-        exit(0)
-    except Exception as e:
-        logging.error(e)
-        browser.quit()
-        exit(1337)
+            sleep(5)
+        except Exception as e:
+            print(e)
+            exit(2)
+        meh_function()
