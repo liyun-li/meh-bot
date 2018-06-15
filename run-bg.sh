6f4a50453956279ca2c489df4252ad7e96b78a9d
Liyun Li <liyun-li@users.noreply.github.com>
2018-06-14 19:23:51 -0700

    Removed a space

--- run-bg.sh
+++ run-bg.sh
@@ -2,5 +2,5 @@
 
 docker build . -t meh-bot --no-cache
 docker rm -f meh-bot 2>/dev/null
-docker run --name meh-bot -d -ti  meh-bot
+docker run --name meh-bot -d -ti meh-bot
 echo 'Meh bot is running. Go do whatever'
