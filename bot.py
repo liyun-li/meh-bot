9c4b666c3f09bf8d2c1637697d3c5023221d9b8d
Liyun Li <liyun-li@users.noreply.github.com>
2018-08-18 15:47:21 -0400

    error handling

--- bot.py
+++ bot.py
@@ -19,7 +19,9 @@ import logging
 # load ".env" file
 load_dotenv(dotenv_path='.env')
 
+
 class Meh:
+
     '''Everything you need in for clicking'''
     # home page
     FLIPPER_SELECTOR = 'div[class="flipper"] button'
@@ -45,11 +47,13 @@ class Meh:
         file_handler.setLevel(logging.INFO)
 
         # custom timezone
-        converter = lambda x, y: (datetime.utcnow() - timedelta(hours=4)).timetuple()
+        converter = lambda x, y: (
+            datetime.utcnow() - timedelta(hours=4)).timetuple()
         logging.Formatter.converter = converter
 
         # create a logging format
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s: %(message)s')
         file_handler.setFormatter(formatter)
 
         # add the handlers to the self.logger
@@ -62,7 +66,7 @@ class Meh:
         browser_choice = getenv('BROWSER_CHOICE')
         if browser_choice not in choices:
             self.browser = www.Firefox(
-                firefox_options=options, 
+                firefox_options=options,
                 executable_path='./geckodriver'
             )
         elif browser_choice.lower() == choices[2]:
@@ -73,18 +77,18 @@ class Meh:
         elif browser_choice.lower() in choices[:2]:
             options.binary_location = '/usr/bin/' + browser_choice
             self.browser = www.Chrome(
-                chrome_options=options, 
+                chrome_options=options,
                 executable_path='./chromedriver'
             )
         else:
             self.logger.error('Browser startup failure')
-            exit(1) # shit happens
+            exit(1)  # shit happens
 
         # driver wait
         self.wait = WebDriverWait(self.browser, 60)  # maximum wait time
         self.logged_in = False
 
-    def text(self):
+    def text_product(elf):
         '''Send pics and title'''
         SEND_SMS = getenv('SEND_SMS')
         if not SEND_SMS:
@@ -93,8 +97,10 @@ class Meh:
 
         # construct message body
         sms_body = 'Meh:\n'
-        sms_body += self.browser.find_element_by_css_selector(self.PRODUCT_TITLE_SELECTOR).text
-        img = self.browser.find_element_by_id(self.PRODUCT_PHOTO_ID).get_attribute('src')
+        sms_body += self.browser.find_element_by_css_selector(
+            self.PRODUCT_TITLE_SELECTOR).text
+        img = self.browser.find_element_by_id(
+            self.PRODUCT_PHOTO_ID).get_attribute('src')
 
         # twilio credentials
         ACCOUNT_SID = getenv('TWILIO_ACCOUNT_SID')
@@ -114,6 +120,32 @@ class Meh:
 
         return 0
 
+    def text_error(self, e):
+        '''Send error'''
+        SEND_SMS = getenv('SEND_SMS')
+        if not SEND_SMS:
+            self.logger.info('Texting not configured')
+            return 1
+
+        # construct message body
+        sms_body = 'ERROR: {}'.format(e)
+
+        # twilio credentials
+        ACCOUNT_SID = getenv('TWILIO_ACCOUNT_SID')
+        AUTH_TOKEN = getenv('TWILIO_AUTH_TOKEN')
+        TO_NUMBER = getenv('TWILIO_TO_NUMBER')
+        FROM_NUMBER = getenv('TWILIO_FROM_NUMBER')
+
+        # Texting
+        self.logger.info('Texting error...')
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+        message = client.messages.create(
+            to=TO_NUMBER,
+            from_=FROM_NUMBER,
+            body=sms_body
+        )
+
+        return 0
 
     def login(self):
         '''Log into Meh'''
@@ -124,8 +156,10 @@ class Meh:
             (By.CSS_SELECTOR, self.SIGNIN_BUTTON_SELECTOR)
         ))
 
-        signin_input_user = self.browser.find_element_by_id(self.SIGNIN_USER_ID)
-        signin_input_pass = self.browser.find_element_by_id(self.SIGNIN_PASS_ID)
+        signin_input_user = self.browser.find_element_by_id(
+            self.SIGNIN_USER_ID)
+        signin_input_pass = self.browser.find_element_by_id(
+            self.SIGNIN_PASS_ID)
 
         signin_input_user.clear()
         signin_input_pass.clear()
@@ -134,7 +168,7 @@ class Meh:
 
         self.logger.info('Signing in...')
         signin_button.click()
-        sleep(3) # give it 3 seconds to authenticate
+        sleep(3)  # give it 3 seconds to authenticate
         if self.SIGNIN not in self.browser.current_url:
             self.logged_in = True
 
@@ -157,6 +191,7 @@ class Meh:
         except Exception as e:
             self.logger.error(e)
             self.browser.quit()
+            self.text_error(e)
             raise
         finally:
             return 0
@@ -166,10 +201,11 @@ class Meh:
         self.logger.info('Visiting homepage...')
         try:
             self.browser.get(self.HOMEPAGE)
-            self.text()
+            self.text_product()
         except Exception as e:
             self.logger.error(e)
             self.browser.quit()
+            self.text_error(e)
             raise
 
     def midnight_fun(self):
@@ -184,11 +220,13 @@ class Meh:
         converted = utc_dt.astimezone(my_tz)
         return converted.timetuple()
 
+
 def seconds_till_tomorrow():
     '''Count the seconds until midnight'''
     global asdf
     n = datetime.utcnow() - timedelta(hours=4)
     return (23 - n.hour) * 3600 + (59 - n.minute) * 60 + (60 - n.second), n
+
 
 def countdown():
     '''Count down until midnight'''
@@ -209,6 +247,9 @@ if __name__ == '__main__':
         self.logger.info('You don\'t have a .env file')
         exit(1)
 
+    # wait time
+    wait_time = 240
+
     # Meh wrapper
     meh = Meh()
 
@@ -223,6 +264,7 @@ if __name__ == '__main__':
 
     while True:
         countdown()
-        meh.logger.info('Countdown is over. Wait 30 seconds...')
-        sleep(150) # to avoid high traffic
+        meh.logger.info(
+            'Countdown is over. Wait {} seconds...'.format(wait_time))
+        sleep(wait_time)  # to avoid high traffic
         meh.midnight_fun()
